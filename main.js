eb335350cc98a3766060a3181b229ee0e831f940
brayanpesantes <brayalexis.30@gmail.com>
2022-04-18 00:39:34 -0500

    agregar funcionalidaes a menu

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const openMenu = document.getElementById('open-menu');
 const closeMenu = document.getElementById('close-menu');
 const navigation = document.getElementById('navigation');
+const items = document.querySelectorAll('.item')
+const subItems = document.querySelectorAll('.subItem')
 
 openMenu.addEventListener('click', () => {
     navigation.classList.add('openMenu');
@@ -10,4 +12,29 @@ openMenu.addEventListener('click', () => {
 closeMenu.addEventListener('click', () => {
     navigation.classList.add('closeMenu');
     navigation.classList.remove('openMenu');
+
 })
+
+items.forEach(item => {
+
+    item.addEventListener('click', () => {
+        removeClassActive();
+        if (!item.classList.contains('active')) {
+            item.classList.add('active');
+            if (item.classList.contains('sub__menu')) {
+                item.childNodes[3].classList.toggle('activeSubMenu');
+                // console.log(item.childNodes)
+                // if (item.childNodes[3]) {
+
+                //     console.log(item.childNodes[3])
+                // }
+            }
+        }
+    })
+})
+
+const removeClassActive = () => {
+    items.forEach(item => {
+        item.classList.remove('active');
+    })
+}
